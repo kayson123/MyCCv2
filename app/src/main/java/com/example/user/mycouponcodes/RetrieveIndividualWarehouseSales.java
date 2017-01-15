f68f4cd8477815cb859bd4616727f68885d38a9a
kayson123 <khlim1993@gmail.com>
2017-01-15 18:27:42 +0800

    changed API URL to live server

--- app/src/main/java/com/example/user/mycouponcodes/RetrieveIndividualWarehouseSales.java
+++ app/src/main/java/com/example/user/mycouponcodes/RetrieveIndividualWarehouseSales.java
@@ -116,7 +116,7 @@ public class RetrieveIndividualWarehouseSales extends AppCompatActivity implemen
 
     class RetrieveItem extends AsyncTask<Void,Void,Void>{
         private ProgressDialog pDialog;
-        private String url = "http://192.168.0.104/mycc/retrieve_individual_warehouse_sales.php";
+        private String url = "http://www.hermosa.com.my/khlim/retrieve_individual_warehouse_sales.php";
         private String TAG = RetrieveIndividualWarehouseSales.RetrieveItem.class.getSimpleName();
         List<WarehouseSalesDetails> data = new ArrayList<>();
         ArrayList<HashMap<String,String>> sales_details;
